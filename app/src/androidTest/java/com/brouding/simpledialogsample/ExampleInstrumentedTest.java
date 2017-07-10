d3ac9d49faee8dd6ebc505b7f98c722dae16a734
Lee Jeongwon <ssyjk2@gmail.com>
2017-07-10 17:12:14 +0900

    - Travis..Right…package name changed before.

--- app/src/androidTest/java/com/brouding/simpledialogsample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/brouding/simpledialogsample/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("brouding.com.simpledialogsample", appContext.getPackageName());
+        assertEquals("com.brouding.simpledialogsample", appContext.getPackageName());
     }
 }
--- simpledialog/src/androidTest/java/com/brouding/simpledialog/ExampleInstrumentedTest.java
+++ simpledialog/src/androidTest/java/com/brouding/simpledialog/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("brouding.com.simpledialog.test", appContext.getPackageName());
+        assertEquals("com.brouding.simpledialog.test", appContext.getPackageName());
     }
 }
